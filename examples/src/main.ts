6c00ef2814597867d8141b6f42ccb5ab92c7caa7
wangbo <781278657@qq.com>
2023-05-09 15:59:17 +0800

    revert: ↪️ da88bf271a157cd2499e227f53e445f4176f21c5

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'bi-chart/style/index.css'
+import 'bi-chart/dist/es/style.css'
 
 createApp(App).mount('#app')
