34f38086ee2ef3ba04382e5418f5ca5dc856270c
wangbo <781278657@qq.com>
2023-05-09 16:29:21 +0800

    feat: ✨ 缓存 pkg。采用开发模式引入包

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import { BasicBar, BasicPie } from 'bi-chart'
+import { BasicPie } from 'bi-chart'
 </script>
 
 <template>
   <div style="width: 600px; height: 400px;">
-    <basic-bar />
     <basic-pie />
   </div>
 </template>
--- examples/src/main.ts
+++ examples/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'bi-chart/dist/es/style.css'
 
 createApp(App).mount('#app')
