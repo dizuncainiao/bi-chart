5cfd48622b7a979294657ac3484faba86b88c0f6
wangbo <781278657@qq.com>
2023-05-12 17:52:32 +0800

    feat: ✨ info 插槽时间统一化

---
+++ packages/bi-chart/components/basic-business/hooks/date.ts
@@ -0,0 +1,18 @@
+import dayjs from 'dayjs'
+
+const YMD = 'YYYY-MM-DD'
+
+export const today = dayjs().format(YMD)
+
+export const yesterday = dayjs().subtract(1, 'day').format(YMD)
+
+export const optionDate = {
+  '0': today,
+  '1': yesterday,
+  '2': dayjs().subtract(7, 'day').format(YMD),
+  '3': dayjs().startOf('week').format(YMD),
+  '4': dayjs().subtract(30, 'day').format(YMD),
+  '5': dayjs().startOf('month').format(YMD)
+}
+
+export type OptionDataKeys = keyof typeof optionDate
