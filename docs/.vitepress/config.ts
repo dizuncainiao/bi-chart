4fab11e0b63f485db22a9f1fdf54aba840f57114
wangbo <781278657@qq.com>
2023-06-05 10:43:27 +0800

    feat: ✨ 移除 pages ignore

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/bi-chart/',
-  outDir: '../pages',
   title: 'BI Chart',
   description: 'BI-Chart 图表组件库',
   themeConfig: {
