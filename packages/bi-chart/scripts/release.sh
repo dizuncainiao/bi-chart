692b0fe4c99accee08679fe077b71b1570cd9af4
wangbo <781278657@qq.com>
2023-06-07 12:44:54 +0800

    feat: ✨ 优化 release.sh

--- packages/bi-chart/scripts/release.sh
+++ packages/bi-chart/scripts/release.sh
@@ -1,13 +1,23 @@
 cd ../publish
 
+# 提升 publish/package.json 版本
 npm version patch
+
+git add .
+
+git commit -m "chore: 🤖 publish/package.json version patch"
 
 cd ../../../
 
+# 提升根目录 package.json 版本
 npm version patch -m "chore: 🤖 v%s"
+
+pnpm run changelog
+
+git add .
+
+git commit -m "docs: 📝 Docs add CHANGELOG.md"
 
 echo "按任意键继续"
 read -n 1
 echo "继续运行"
-
-pnpm run changelog
