b755cdec9c014e955b8866e205471bf5532d072a
wangbo <781278657@qq.com>
2023-08-09 18:01:15 +0800

    docs: 📝 折线图文档

---
+++ docs/demos/basic/line/BasicLineDemo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { BasicChart, allEchartsOptions } from 'dz-bi-chart'
+
+defineOptions({
+  name: 'BasicLineDemo'
+})
+</script>
+
+<template>
+  <div
+    style="
+      height: 400px;
+      padding: 28px;
+      background: #fff;
+      border-radius: 12px;
+      box-shadow: 0 0 8px #eee;
+"
+  >
+    <BasicChart :options="allEchartsOptions.basicLine" />
+  </div>
+</template>
