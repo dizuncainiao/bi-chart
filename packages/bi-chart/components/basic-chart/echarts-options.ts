f2f4bec8254d22dfeedc9669b73c7430544f9cad
wangbo <781278657@qq.com>
2023-08-10 15:57:35 +0800

    feat: ✨ 清空图表的默认展示数据

--- packages/bi-chart/components/basic-chart/echarts-options.ts
+++ packages/bi-chart/components/basic-chart/echarts-options.ts
@@ -6,8 +6,23 @@ export type AllChartOptions = typeof allEchartsOptions
 
 export type ChartType = keyof AllChartOptions
 
+// 清空图表的默认数据
+export function wipeOptionData(options: any) {
+  const option = cloneDeep(options)
+  if (Array.isArray(option.series)) {
+    option.series.forEach((item: any) => {
+      item.data = []
+    })
+  } else if (
+    Object.prototype.toString.call(option.series) === '[object Object]'
+  ) {
+    option.series.data = []
+  }
+  return option
+}
+
 export function getChartOption(type: ChartType) {
-  const option = cloneDeep(Reflect.get(allEchartsOptions, type))
+  const option = wipeOptionData(Reflect.get(allEchartsOptions, type))
   return ref(option)
 }
 
