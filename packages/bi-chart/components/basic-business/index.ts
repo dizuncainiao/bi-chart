2eb4c6ec9d8fc93837b30c93fa9e06bde1e39471
wangbo <781278657@qq.com>
2023-08-10 09:39:23 +0800

    feat: ✨ 暴露 DepSelect、DateSelect

--- packages/bi-chart/components/basic-business/index.ts
+++ packages/bi-chart/components/basic-business/index.ts
@@ -1,6 +1,8 @@
 import { App } from 'vue'
 import basicBusinessLayout from './BasicBusinessLayout.vue'
 import basicSmallLayout from './BasicSmallLayout.vue'
+import dateSelect from './date-select/DateSelect.vue'
+import depSelect from './dep-select/DepSelect.vue'
 
 const BasicBusinessLayout = Object.assign(basicBusinessLayout, {
   install: (app: App) =>
@@ -11,4 +13,17 @@ const BasicSmallLayout = Object.assign(basicSmallLayout, {
   install: (app: App) => app.component(basicSmallLayout.name, basicSmallLayout)
 })
 
-export { BasicBusinessLayout as default, BasicSmallLayout }
+const DateSelect = Object.assign(dateSelect, {
+  install: (app: App) => app.component(dateSelect.name, dateSelect)
+})
+
+const DepSelect = Object.assign(depSelect, {
+  install: (app: App) => app.component(depSelect.name, depSelect)
+})
+
+export {
+  BasicBusinessLayout as default,
+  BasicSmallLayout,
+  DepSelect,
+  DateSelect
+}
